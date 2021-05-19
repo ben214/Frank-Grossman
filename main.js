1cca73b0e9224061735b38d7d6e2972bbe940a0c
ben214 <80927447+ben214@users.noreply.github.com>
2021-05-19 12:04:54 +0300

    basic styles

---
+++ main.js
@@ -0,0 +1,10 @@
+var barItem = document.getElementById("top-bar-item");
+// document.addEventListener("mouseover", rolldown);
+var dropping = document.getElementsByClassName('dropping');
+barItem.addEventListener("mouseover", roll) 
+
+function roll(){
+    console.log("dropping.style");
+  
+
+}
