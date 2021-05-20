141b3bb9cd06cbe78db1495bed36bfcd8cbf613f
ben214 <80927447+ben214@users.noreply.github.com>
2021-05-20 17:12:00 +0300

    top menu bar first got it right with the divs and a's, top-bar-items have a width

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-var barItem = document.getElementById("top-bar-item");
-// document.addEventListener("mouseover", rolldown);
-var dropping = document.getElementsByClassName('dropping');
-barItem.addEventListener("mouseover", roll) 
+// var barItem = document.getElementsByClassName("topBarItem");
+// // document.addEventListener("mouseover", rolldown);
+// var dropping = document.getElementsByClassName('dropping');
+// barItem.addEventListener("mouseover", roll);
+// barItem.addEventListener("mouseout", hide) 
 
-function roll(){
-    console.log("dropping.style");
-  
-
-}
+// function roll(){
+//     console.log("dropping");
+//     $(".dropping").toggleClass("hide");
+// }
+// function hide(){
+//     console.log("dropping");
+//     $(".dropping").toggleClass("hide");
+// }
