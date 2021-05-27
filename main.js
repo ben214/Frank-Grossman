d810c3eb98a2165c3a4471f1160708f06ba7a6fd
ben214 <80927447+ben214@users.noreply.github.com>
2021-05-27 19:47:21 +0300

    links from bar functioning good. not yet getting to top at press.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 // }
 
 $(document).ready(function(){
- 
+    $(".collapse").click(collapse)
   
  $(".mamarlist>h1").click(expand);
 function expand() {
@@ -28,4 +28,17 @@ function collapse() {
     $(".mamarlist>h1").on("click", expand)
   };
 
+
+//   var terget = 
+  $(".refer").click(expandtarget)
+  function expandtarget() {
+      var targeted = $(this).attr('href');
+      var $id = targeted;
+      var $parent = $($id).parent;
+      $($id).trigger('click')
+
+  };
 });
+
+      
+     
