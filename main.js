e7c8bdcfe4aec30d24149bf6caaddcfd76515974
ben214 <80927447+ben214@users.noreply.github.com>
2021-05-30 13:31:45 +0300

    הוספת מאמרים הושלמה, קישורים למאמרים עובדים, עוד לא מופיעים למעלה.

--- main.js
+++ main.js
@@ -1,43 +1,53 @@
 
-// function Mopen(event) {
-    
-//     $(".mamar-content").toggleClass("blockk")
-//     // mcontent.style.opacity = 1;
-    
-//     console.log($(".mamar-content"));
-// }
-
 $(document).ready(function(){
-    $(".collapse").click(collapse)
-  
- $(".mamarlist>h1").click(expand);
+
+$(".collapse").click(collapsee),
+ $(".mamarlist").click(expand)
+
 function expand() {
-   $(this).parent(".mamarlist").css({
-       "height": "auto",
-       "max-height": "3000px",
-       "font-size" :"1.5em"
-   }),
-   $(this).on("click", collapse),
-   $(".collapse").click(collapse)
- };
-     
+    var targeted = $(this).attr('id');
+    var $id = '#'+targeted;
+    $(".mamarlist").css({
+        'height': 'auto',
+        'maxHeight': '80px',
+        'fontSize': '19.2px'
+    })
+    $(this).css({
+        'height': 'auto',
+        'maxHeight': '3000px',
+       'fontSize': '1.5em'
+      
+    });
+    $(this).click(collapse),
+    console.log($(this).css("zIndex"));
+};
+
 function collapse() {
-    $(this).closest(".mamarlist").css({
-        "max-height": "80px",
-        "font-size" :"1em"});
-    $(".mamarlist>h1").on("click", expand)
+    $(this).css({
+        'height': 'auto',
+        'maxHeight': '80px',
+       'fontSize': '19.2px'
+    });
+    $(".mamarlist").click(expand),
+    $(this).click(expand),
+    console.log($(this).css("fontSize"))
   };
 
-
-//   var terget = 
-  $(".refer").click(expandtarget)
+$(".refer").click(expandtarget)
   function expandtarget() {
       var targeted = $(this).attr('href');
       var $id = targeted;
-      var $parent = $($id).parent;
-      $($id).trigger('click')
+      
+      $($id).trigger('click');
+      console.log("refer")
+    };
 
-  };
+    function collapsee() {
+        $(this).closest(".mamarlist").trigger(click),
+        $(".mamarlist").click(expand)
+      };
+
+  
 });
 
       
